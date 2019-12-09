f4e1dcfbbd76d8eef2ec840fdbb769632c9824af
Wangfei MA <Wangfei.MA@ucsf.edu>
2019-12-08 23:24:23 -0800

    Update multialign.py

--- scripts/multialign.py
+++ scripts/multialign.py
@@ -12,6 +12,12 @@ manager = mul.Manager()
 lock1 = mul.Lock()
 
 
+class metapath:
+	
+	def __init__(self,metafile):
+		
+		meta=pd.read_csv(metafile,sep='\t',header=None,usecols=range(10))
+		self.pathes={x:path for x,path in zip(list(meta[0]),list(meta[4]))}
 
 def find_breaks(anchor_sizes,seq):
 
